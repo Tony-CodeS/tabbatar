2d6faef2aa6992335d02ed825e2779b5fcac689e
Tony <asolomon484@gmail.com>
2023-03-24 14:04:15 +0100

    MOVAZZ

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div>
+        <div>
+            <h2>movazz</h2>
+            <div>
+                <ul>
+                 
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default navbar
